60d30842779d99765254d2ef218b234286894645
rahul <rahul.kumar@housing.com>
2015-10-26 19:00:23 +0530

    Small changes in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 setup(name='rmqreceiver',
       version='0.1',
       description='Rabbitmq Receiver',
-      url='http://10.1.6.107:8034/docs/_build/html/common.html#module-common.rabbitmq_receiver',
+      url='http://rahulkumar.housing.com:8034/docs/_build/html/common.html#module-common.rabbitmq_receiver',
       author='Rahul Kumar',
       author_email='rahul.kumar@housing.com',
       license='MIT',
