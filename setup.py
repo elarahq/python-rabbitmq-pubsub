8d36cc7311de159937099605681c1527c9e447e9
rahul <rahul.kumar@housing.com>
2015-12-04 19:47:14 +0530

    rmqproducer package

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def readme():
         return f.read()
 
 setup(name='rmqreceiver',
-      version='0.1.2',
+      version='0.1.3',
       description='Rabbitmq Receiver',
       url='https://github.com/loconsolutions/python.rabbitmq.receiver',
       author='Rahul Kumar',
       author_email='rahul.kumar@housing.com',
       license='MIT',
-      packages=['rmqreceiver'],
+      packages=['rmqreceiver', 'rmqproducer'],
       install_requires=[
           'pika',
       ],
